283da05988ffd6cc15500bf1bc817d55a9bae083
Radomir Skrzepij <zyziu10@gmail.com>
2019-02-11 21:47:09 +0100

    init travis CI (#3)
    * init travis CI
    
    * change nodejs version for travis
    
    * fix tests
    
    * fix lint
    
    * add testing script to travis ci
    
    * typo fix in test script definition
    
    * add e2e test to travis
    
    * GH pages deploy via travis
    
    * add footer - just to check if deploy works properly

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed, async } from '@angular/core/testing'
 import { RouterTestingModule } from '@angular/router/testing'
 import { AppComponent } from './app.component'
+import { MatIconModule, MatListModule } from '@angular/material'
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        MatIconModule,
+        MatListModule,
       ],
       declarations: [
         AppComponent
