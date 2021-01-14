0c20dd77eaa4b245c05b78bb1ff295afbb71365a
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-01-14 07:10:05 +0000

    Deploy skrzepij/skrzepij.github.io to github.com/skrzepij/skrzepij.github.io.git:master

--- component---src-pages-index-tsx-91bd86f9f35d9d5f7bd0.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{QeBL:function(n,e,t){"use strict";t.r(e);var o=t("q1tI"),l=t.n(o);e.default=function(){return l.a.createElement("div",null,"Hello world!")}}}]);
-//# sourceMappingURL=component---src-pages-index-tsx-91bd86f9f35d9d5f7bd0.js.map
