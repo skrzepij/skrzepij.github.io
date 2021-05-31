906690919584d9e023d17cdcb8df03d6e5bea963
Radomir Skrzepij <zyziu10@gmail.com>
2021-05-31 08:29:58 +0200

    Add google analytics v4 support (#46)

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,5 +64,14 @@ module.exports = {
         sampleRate: 0.7,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'G-KL8QDZ29XV', // Google Analytics / GA
+        ],
+      },
+    },
   ],
 }
