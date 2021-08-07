9b81dc0f3c8ceeeb0d01b8bfc97f810d091722ec
Radomir Skrzepij <radek.s87@gmail.com>
2021-08-07 14:26:07 +0200

    add photo component and shadow changes to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,15 @@ module.exports = {
       ...colors,
       'black-yellow': '#141000',
     },
+    boxShadow: {
+      picture: '5px 5px 10px #000',
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
+  corePlugins: {
+    dropShadow: false,
+  },
 }
