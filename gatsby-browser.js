5cf2cb149e3896109cdf7ed35b026aa803408001
Radomir Skrzepij <zyziu10@gmail.com>
2021-08-13 21:55:02 +0200

    Add Montserrat and Roboto fonts; Create some base.scss styles (#57)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,8 @@
+import '@fontsource/montserrat/400.css'
+import '@fontsource/montserrat/600.css'
+import '@fontsource/montserrat/700.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+
 import './src/styles/tailwind.scss'
+import './src/styles/base.scss'
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,18 @@ module.exports = {
   purge: ['./src/**/*.{ts,tsx}'],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        md: '1.05rem',
+      },
+    },
     colors: {
       ...colors,
       'black-yellow': '#141000',
+    },
+    fontFamily: {
+      Roboto: ['Roboto', 'sans-serif'],
+      Montserrat: ['Montserrat', 'sans-serif'],
     },
     boxShadow: {
       picture: '5px 5px 10px #000',
